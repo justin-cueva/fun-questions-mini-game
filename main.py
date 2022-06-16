689f316c31eeb23820140bc4c44b2b5b54fd6284
Justin Cueva <justincueva@Justins-MBP.lan>
2022-06-16 16:27:59 -0700

    checking answers and keeping score

--- main.py
+++ main.py
@@ -8,21 +8,19 @@ for question in question_data:
 
 quiz_brain_1 = QuizBrain(question_bank)
 
-# print(vars(quiz_brain_1.current_question))
-# 2:30
+# print('You got it right!')
+# print('The correct answer was: True.')
+# print('Your current score is: 1/1.')
 
-game_is_on = True
-while game_is_on:
+while quiz_brain_1.game_is_on:
     question_number = quiz_brain_1.current_question_number
     question = quiz_brain_1.current_question.question
     user_input = input(f'Q.{question_number}: {question} (True/False)?: ')
     if user_input == 'quit':
-        break
+        quiz_brain_1.terminate()
     elif user_input == 'restart':
         quiz_brain_1.restart()
-    elif user_input.lower() == 'true':
-        quiz_brain_1.move_to_next_question()
-    elif user_input.lower() == 'false':
-        quiz_brain_1.move_to_next_question()
+    elif user_input.lower() == 'true' or user_input.lower() == 'false':
+        quiz_brain_1.move_to_next_question(user_input)
     else:
         print('not a valid command')
--- quiz_brain.py
+++ quiz_brain.py
@@ -4,11 +4,31 @@ class QuizBrain:
         self.current_question_number = 1
         self.current_question = self.questions_list[self.current_question_number - 1]
         self.quiz_length = len(questions_list)
+        self.game_is_on = True
+        self.score = 0
 
-    def move_to_next_question(self):
+    def check_answer(self, users_answer, correct_answer):
+        answered_correct = users_answer.lower() == correct_answer.lower()
+        if answered_correct:
+            self.score += 1
+        print(f'You got it {"RIGHT" if answered_correct else "WRONG"}!')
+        print(f'The correct answer was: {correct_answer}.')
+        print(f'Your current score is: {self.score}/{self.current_question_number}.')
+        print('\n')
+        if self.current_question_number == self.quiz_length:
+            self.game_is_on = False
+
+    def move_to_next_question(self, users_answer):
+        self.check_answer(users_answer, self.current_question.answer)
+        if not self.game_is_on:
+            return
         self.current_question_number += 1
         self.current_question = self.questions_list[self.current_question_number - 1]
 
+    def terminate(self):
+        self.game_is_on = False
+
     def restart(self):
+        self.score = 0
         self.current_question_number = 1
         self.current_question = self.questions_list[self.current_question_number - 1]
